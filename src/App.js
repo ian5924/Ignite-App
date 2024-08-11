cfd0a3f2ccbf8369d34e0214e1acf0965ec02927
ian5924 <ianwakayu@gmail.com>
2024-08-10 19:35:15 -0700

    Fetched Game Details

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//Components and pages
+import Home from "./pages/Home";
+import GlobalStyles, { createGlobalStyle } from "./components/GlobalStyles";
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
