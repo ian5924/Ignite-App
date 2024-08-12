e541595f5f2579f0180d783a22ffb78b05121394
ian5924 <ianwakayu@gmail.com>
2024-08-11 19:23:21 -0700

    Added page routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 //Components and pages
 import Home from "./pages/Home";
 import GlobalStyles, { createGlobalStyle } from "./components/GlobalStyles";
+import { Route, Routes } from "react-router-dom";
+//<Route path={["/game/:id", "/"]} >
+
+// Allows me to route to home and game/32664 at the same  time
+//<Home />
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Home />
+      <Routes>
+        <Route path="/game/:id" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { legacy_createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
 import { thunk } from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = legacy_createStore(
@@ -19,7 +20,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
