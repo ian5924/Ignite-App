c4e605df44449c942cb2dd77538c15b12edbab8f
ian5924 <ianwakayu@gmail.com>
2024-08-11 20:35:30 -0700

    Fixed toggling

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,7 +10,9 @@ import { Link } from "react-router-dom";
 
 const Game = ({ name, released, image, id }) => {
   const dispatch = useDispatch();
+  //LoadDetailHandler
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
   return (
@@ -30,6 +32,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 1rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
